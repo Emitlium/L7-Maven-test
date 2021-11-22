cb531915fce23f6c0555789c37b2bdb9e5912485
Michaela <88110578+Emitlium@users.noreply.github.com>
2021-11-22 17:43:08 +0100

    Pridani souboru

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Lekce 7 - test");
+    }
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+    @Test
+    public void testSum(){
+        Main main = new Main();
+        assertEquals(7,main.sum(2,5));
+    }
+}
